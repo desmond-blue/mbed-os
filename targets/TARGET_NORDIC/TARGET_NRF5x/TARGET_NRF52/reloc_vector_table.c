3d2fa535a71b7bad08c6a91a31d74f4492c8948c
Naveen Kaje <Naveen.Kaje@arm.com>
2018-09-26 15:26:45 -0500

    NRF52: Fix vector table
    Ensure that vector table gets initialized properly. The table that we
    initialize in startup_nrf52840.S gets wiped out as the section is
    declared as noinit. Fix this by implementing the weak function mbed_sdk_init
    that inits the vector table.

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/reloc_vector_table.c
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/reloc_vector_table.c
@@ -47,15 +47,7 @@
 #include "nrf_dfu_mbr.h"
 #endif
 
-#if defined(__CC_ARM) || (defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050))
-    __attribute__ ((section(".bss.nvictable"),zero_init))
-    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
-#elif defined(__GNUC__)
-    __attribute__ ((section(".nvictable")))
-    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
-#elif defined(__ICCARM__)
-    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS] @ ".nvictable";
-#endif
+uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
 
 extern uint32_t __Vectors[];
 
@@ -113,12 +105,13 @@ void nrf_reloc_vector_table(void)
 #endif
 }
 
-#if (STDIO_UART_RTS != NC)
 void mbed_sdk_init(void)
 {
+#if (STDIO_UART_RTS != NC)
 	gpio_t rts;
 	gpio_init_out(&rts, STDIO_UART_RTS);
 	/* Set STDIO_UART_RTS as gpio driven low */
 	gpio_write(&rts, 0);
-}
 #endif
+	nrf_reloc_vector_table();
+}
